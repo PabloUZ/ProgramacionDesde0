803a781879d786aa1c0412b5b1a631863a992cbc
Pablo Uribe <puribezuluaga28@gmail.com>
2022-03-29 09:18:57 -0500

    Se c modificó la barra de navegacion principal - desplegable

--- style-format/btn.js
+++ style-format/btn.js
@@ -9,6 +9,17 @@ $(window).ready(function() {
         list.style.display="block";
     }
 });
+$(window).ready(function() {
+    var btn=document.getElementById('btn-main-nav');
+    var list=document.getElementById('main-nav');
+    if($(window).outerWidth()<700){
+        btn.style.display="block";
+        list.style.display="none";
+    }else{
+        btn.style.display="none";
+        list.style.display="flex";
+    }
+});
 $(window).resize(function() {
     var btn=document.getElementById('btn-toggle');
     var list=document.getElementById('toggle');
@@ -20,6 +31,17 @@ $(window).resize(function() {
         list.style.display="block";
     }
 });
+$(window).resize(function() {
+    var btn=document.getElementById('btn-main-nav');
+    var list=document.getElementById('main-nav');
+    if($(window).outerWidth()<700){
+        btn.style.display="block";
+        list.style.display="none";
+    }else{
+        btn.style.display="none";
+        list.style.display="flex";
+    }
+});
 function toggle(){
     var list=document.getElementById('toggle');
     if(list.style.display=="none"){
@@ -27,5 +49,17 @@ function toggle(){
     }
     else{
         list.style.display="none";
+    }
+}
+function mainNavToggle(){
+    var btn=document.getElementById('btn-main-nav');
+    var list=document.getElementById('main-nav');
+    if(list.style.display=="none"){
+        list.style.display="flex";
+        btn.innerHTML="&Lambda;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;PERFIL";
+    }
+    else{
+        list.style.display="none";
+        btn.innerHTML="V&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;PERFIL";
     }
 }
